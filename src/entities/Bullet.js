d4c42cffb4b84cef6b99c23095f6326eb3c11b8a
Miitch <miitch@inven.co.kr>
2026-05-12 13:01:02 +0900

    불필요파일 제거

--- src/entities/Bullet.js
+++
@@ -1,8 +0,0 @@
-/**
- * Bullet 엔티티 - Phase 3에서 구현
- */
-export default class Bullet {
-  constructor(scene) {
-    this.scene = scene;
-  }
-}
--- src/entities/Laser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Laser 엔티티 - Phase 3에서 구현
- */
-export default class Laser {
-  constructor(scene) {
-    this.scene = scene;
-  }
-}
